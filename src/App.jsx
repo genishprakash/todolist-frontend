6bb089e681c6e5b53b114090c587b447aeafae0b
Genish Prakash <genishprakash@gmail.com>
2024-05-18 01:20:09 +0530

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { useEffect } from 'react'
 function App() {
   
   const [count, setCount] = useState(0)
-  
   const base_url = import.meta.env.VITE_REACT_APP_SERVER_BASE_URL=== 'development' ? import.meta.env.VITE_REACT_APP_LOCAL_BASE_URL : import.meta.env.VITE_REACT_APP_SERVER_BASE_URL;
+  console.log(base_url)
   useEffect(() => {
     const fetchData = async () => {
       try {
