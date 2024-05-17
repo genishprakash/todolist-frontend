ecd7af1b3c5da00254e36626cc8603e3064e4800
Genish Prakash <genishprakash@gmail.com>
2024-05-18 00:07:49 +0530

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  
   const [count, setCount] = useState(0)
-  const base_url = process.env.REACT_APP_SERVER_BASE_URL;
-   axios.get(`${base_url}/`).then(res=>{setCount(res.data)}).catch(err => alert(`Some error occured ==>${err}`));
+
+  const base_url = import.meta.VITE_REACT_APP_NODE_ENV === 'development' ? import.meta.VITE_REACT_APP_LOCAL_BASE_URL : import.meta.VITE_REACT_APP_SERVER_BASE_URL;
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/`); 
+        const { value } = response.data;
+        
+        setCount(value);
+        
+      } catch (error) {
+        console.log(`Some error occurred: ${error.message}`);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
-      <p>{count}</p>
+      <p style={{fontSize:"2rem", color:"red"}}>{count}</p>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  define: {
+    'process.env.url': `"${process.env.url}"`,
+  },
   server: {
     port: 3000,
-  },
-})
+  }
+});
